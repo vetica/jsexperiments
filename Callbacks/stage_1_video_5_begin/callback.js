172c8fdae86d101391be575f7818eee7039d907b
Spencer Holladay <spencer.holladay@gmail.com>
2019-05-26 19:58:02 -0400

    adds fat arrow functions

--- Callbacks/stage_1_video_5_begin/callback.js
+++ Callbacks/stage_1_video_5_begin/callback.js
@@ -1,13 +1,15 @@
-function executeCallback( callback ) {
+function executeCallback(callback) {
   callback();
 }
 
+
+
 // Print Hello
-executeCallback(function () {
-    console.log('Hello');
-});
+executeCallback(() =>
+  console.log('Hello')
+);
 
 // Print Goodbye
-executeCallback(function () {
-    console.log('Goodbye');
-});
+executeCallback(() =>
+  console.log('Goodbye')
+);
