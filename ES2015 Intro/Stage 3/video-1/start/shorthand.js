c761c22d76410c0cc3da924a66b389b7d8e7d203
Spencer Holladay <spencer.holladay@gmail.com>
2019-05-27 10:52:26 -0400

    adds Stage 3 lessons

---
+++ ES2015 Intro/Stage 3/video-1/start/shorthand.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function submit(name, comments, rating = 5) {
+  let data = { name, comments, rating };
+  
+  for (let key in data) {
+    console.log(key + ':', data[key]);
+  }
+  // … do ajax request
+}
+              
+//
