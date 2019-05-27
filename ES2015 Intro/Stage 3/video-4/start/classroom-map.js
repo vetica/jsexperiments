f355e469802e1430fa9ebc32dd2a00872b7ec87a
Spencer Holladay <spencer.holladay@gmail.com>
2019-05-27 11:17:23 -0400

    learning Maps

--- ES2015 Intro/Stage 3/video-4/start/classroom-map.js
+++ ES2015 Intro/Stage 3/video-4/start/classroom-map.js
@@ -1,3 +1,19 @@
 'use strict';
 
 let classroom = new Map();
+
+let stevenJ = { name: 'Steven', age: 22 },
+  sarah = { name: 'Sarah', age: 23 },
+  stevenS = { name: 'Steven', age: 22 };
+
+classroom.set('stevenJ', stevenJ);
+classroom.set('sarah', sarah);
+classroom.set('stevenS', stevenS);
+
+console.log('classroom size', classroom.size);
+
+if (classroom.has('stevenJ')) console.log('stevenJ is in the classroom');
+if (classroom.has('sarah')) console.log('sarah is in the classroom');
+if (classroom.has('stevenS')) console.log('stevenS is in the classroom');
+
+// console.log('sarah', classroom.get('sarah'));
